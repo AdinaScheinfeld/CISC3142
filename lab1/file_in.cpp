6d8958467495358ba562ab926ca7ee216e72c2b1
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-02-10 16:52:13 -0500

    add functions to calculate price per brand and price per category

--- lab1/file_in.cpp
+++ lab1/file_in.cpp
@@ -20,9 +20,17 @@ All programs must be able to compile in C++98 standard (the default version on L
 #include <sstream>
 #include <vector>
 #include <fstream>
+#include <map>
+#include <iterator>
 
 // define namespace
 using namespace std;
+
+// declare pricePerBrand function
+void pricePerBrand(vector<string> brands, vector<float> prices);
+
+// declare pricePerCategory function
+void pricePerCategory(vector<string> categories, vector<float> prices);
 
 int main()
 {
@@ -75,6 +83,7 @@ int main()
       vYear.push_back(iYear);
       vPrice.push_back(fPrice);
     }
+
     // close the file cout << "Number of entries: " << i-1 << endl;
     in_stream.close();
   }
@@ -101,5 +110,104 @@ int main()
     cout << vSKU[j] << "\t" << vBrand[j] << "\t   " << vCategory[j] << "\t\t" << vYear[j] << "\t" << vPrice[j] << endl;
   }
 
+  // print a blank line
+  cout << '\n';
+
+  // call pricePerBrand function
+  pricePerBrand(vBrand, vPrice);
+
+  // call pricePerCategory function
+  pricePerCategory(vCategory, vPrice);
+
+  cout << endl;
+}
+
+// function to calculate price per brand
+void pricePerBrand(vector<string> brands, vector<float> prices)
+{
+  // declare map to hold the average per brand
+  map<string, float> averages;
+
+  // add each brand to the map
+  for (int i = 0; i < brands.size(); i++)
+  {
+    averages.insert(pair<string, float>(brands[i], 0));
+  }
+
+  // iterate through the map
+  map<string, float>::iterator itr;
+  for (itr = averages.begin(); itr != averages.end(); ++itr)
+  {
+    string b = itr->first;
+    float sum = 0;
+    int count = 0;
+
+    // calculate the sum and count for each brand
+    for (int j = 0; j < brands.size(); j++)
+    {
+      if (brands[j] == b)
+      {
+        sum += prices[j];
+        count++;
+      }
+    }
+
+    // add the average for each brand to the map
+    map<string, float>::iterator it = averages.find(b);
+    if (it != averages.end())
+      it->second = sum / count;
+  }
+
+  // print the average for each brand to the console
+  cout << "\tBrand\tAverage\n";
+  for (itr = averages.begin(); itr != averages.end(); ++itr)
+  {
+    cout << '\t' << itr->first << '\t' << itr->second << '\n';
+  }
+  cout << endl;
+}
+
+// function to calculate price per category
+void pricePerCategory(vector<string> categories, vector<float> prices)
+{
+  // declare map to hold the average per category
+  map<string, float> averages;
+
+  // add each category to the map
+  for (int i = 0; i < categories.size(); i++)
+  {
+    averages.insert(pair<string, float>(categories[i], 0));
+  }
+
+  // iterate through the map
+  map<string, float>::iterator itr;
+  for (itr = averages.begin(); itr != averages.end(); ++itr)
+  {
+    string b = itr->first;
+    float sum = 0;
+    int count = 0;
+
+    // calculate the sum and count for each category
+    for (int j = 0; j < categories.size(); j++)
+    {
+      if (categories[j] == b)
+      {
+        sum += prices[j];
+        count++;
+      }
+    }
+
+    // add the average for each category to the map
+    map<string, float>::iterator it = averages.find(b);
+    if (it != averages.end())
+      it->second = sum / count;
+  }
+
+  // print the average for each category to the console
+  cout << "\tCategory\tAverage\n";
+  for (itr = averages.begin(); itr != averages.end(); ++itr)
+  {
+    cout << '\t' << itr->first << '\t' << '\t' << itr->second << '\n';
+  }
   cout << endl;
 }
