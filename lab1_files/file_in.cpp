a6863195cdd4ffbf5d1dff88c6c15d260cbde209
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-03-02 20:01:40 -0500

    Refactor code to use header file and fprintf

--- lab1_files/file_in.cpp
+++ lab1_files/file_in.cpp
@@ -10,6 +10,7 @@
 */
 
 // include required libraries
+#include <stdio.h>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
@@ -19,38 +20,41 @@
 #include <fstream>
 #include <map>
 #include <iterator>
+#include <typeinfo>
+#include "header_file.h"
 
 // define namespace
 using namespace std;
 
 // declare pricePerBrand function
-void pricePerBrand(vector<string> brands, vector<float> prices, ofstream& out_stream);
+/// void pricePerBrand(vector<string> brands, vector<float> prices, ofstream& out_stream);
 
 // declare pricePerCategory function
-void pricePerCategory(vector<string> categories, vector<float> prices, ofstream& out_stream);
+/// void pricePerCategory(vector<string> categories, vector<float> prices, ofstream& out_stream);
 
 // declare skusPerYear function
-void skusPerYear(vector<int> years, vector<int> skus, ofstream& out_stream);
+/// void skusPerYear(vector<int> years, vector<int> skus, ofstream& out_stream);
 
 
 // main function
 int main()
 {
   // define vector variables
-  string sku, brand, category, year, price;
-  vector<int> vSKU;
-  vector<string> vBrand;
-  vector<string> vCategory;
-  vector<int> vYear;
-  vector<float> vPrice;
+  // string sku, brand, category, year, price;
+  // vector<int> vSKU;
+  // vector<string> vBrand;
+  // vector<string> vCategory;
+  // vector<int> vYear;
+  // vector<float> vPrice;
 
   // open the input file 
   ifstream in_stream;
   in_stream.open("lab1_files/data.csv");
 
   // open the output file
-  ofstream out_stream;
-  out_stream.open("lab1_files/output.txt");
+  // ofstream out_stream;
+  // out_stream.open("lab1_files/output.txt");
+  FILE *fptr = fopen("lab1_files/output.txt", "w");
 
   // if the file is open
   if (!in_stream.fail())
@@ -65,11 +69,11 @@ int main()
     while (getline(in_stream, line))
     {
       // declare variables to hold the data in each column
-      int iSKU;
-      string sBrand;
-      string sCategory;
-      int iYear;
-      float fPrice;
+      // int iSKU;
+      // string sBrand;
+      // string sCategory;
+      // int iYear;
+      // float fPrice;
 
       // replace commas with spaces
       replace(line.begin(), line.end(), ',', ' ');
@@ -97,53 +101,72 @@ int main()
   // if the file cannot be opened, print an error message to the console
   else
   {
-    out_stream << "Unable to open file";
+    // out_stream << "Unable to open file";
+    fprintf(fptr, "%s", "Unable to open file.");
   }
 
   // write the header values to the output file
-  out_stream << "SKU"
-       << "\t"
-       << "Brand"
-       << "\t"
-       << "Category"
-       << "\t\t"
-       << "Year"
-       << "\t"
-       << "Price" << endl;
+  // out_stream << "SKU"
+  //      << "\t"
+  //      << "Brand"
+  //      << "\t"
+  //      << "Category"
+  //      << "\t\t"
+  //      << "Year"
+  //      << "\t"
+  //      << "Price" << endl;
+
+  fprintf(fptr, "%s\t%s\t%s\t\t%s\t%s\n", "SKU", "Brand", "Category", "Year", "Price");
+
 
   // write the data values to the output file
-  for (int j = 0; j < vSKU.size(); j++)
+  for (int j=0; j<vSKU.size(); j++)
   {
-    out_stream << fixed << setprecision(2) << 
-    vSKU[j] << "\t" << 
-    vBrand[j] << "\t   " << 
-    vCategory[j] << "\t\t" << 
-    vYear[j] << "\t" << 
-    vPrice[j] << endl;
+    // out_stream << fixed << setprecision(2) << 
+    // vSKU[j] << "\t" << 
+    // vBrand[j] << "\t   " << 
+    // vCategory[j] << "\t\t" << 
+    // vYear[j] << "\t" << 
+    // vPrice[j] << endl;
+    fprintf(fptr, "%i\t%s\t\t\t%s\t\t\t%i\t%4.2f\n", 
+    vSKU[j], vBrand[j].c_str(), vCategory[j].c_str(), vYear[j], vPrice[j]);
+
   }
 
   // print a blank line
-  out_stream << '\n';
+  //out_stream << '\n';
+  fprintf(fptr, "\n");
 
   // call pricePerBrand function
-  pricePerBrand(vBrand, vPrice, out_stream);
+  // pricePerBrand(vBrand, vPrice, out_stream);
+  pricePerBrand(vBrand, vPrice, fptr);
+
 
   // call pricePerCategory function
-  pricePerCategory(vCategory, vPrice, out_stream);
+  // pricePerCategory(vCategory, vPrice, out_stream);
+  pricePerCategory(vCategory, vPrice, fptr);
+
 
   // call skusPerYear function
-  skusPerYear(vYear, vSKU, out_stream);
+  // skusPerYear(vYear, vSKU, out_stream);
+  skusPerYear(vYear, vSKU, fptr);
+
 
   // indicate the end of the output
-  out_stream << endl;
+  // out_stream << endl;
 
   // close the output file
-  out_stream.close();
+  // out_stream.close();
+  fclose(fptr);
+
+  // indicate successful completion
+  return (0);
 }
 
 
 // function to calculate price per brand
-void pricePerBrand(vector<string> brands, vector<float> prices, ofstream& out_stream)
+// void pricePerBrand(vector<string> brands, vector<float> prices, ofstream& out_stream)
+void pricePerBrand(vector<string> brands, vector<float> prices, FILE *fptr)
 {
   // declare map to hold the average price per brand
   map<string, float> averages;
@@ -179,17 +202,23 @@ void pricePerBrand(vector<string> brands, vector<float> prices, ofstream& out_st
   }
 
   // write the average for each brand to the output file
- out_stream << "\tBrand\tAverage\n";
+  // out_stream << "\tBrand\tAverage\n";
+  fprintf(fptr, "%s\t%s\n", "Brand", "Average");
   for (itr = averages.begin(); itr != averages.end(); ++itr)
   {
-   out_stream << fixed << setprecision(2) << '\t' << itr->first << '\t' << itr->second << '\n';
+   // out_stream << fixed << setprecision(2) << '\t' << itr->first << '\t' << itr->second << '\n';
+   fprintf(fptr, "%s\t\t%4.2f\n", itr->first.c_str(), itr->second);
   }
-  out_stream << endl;
+
+  // print a new line
+  fprintf(fptr, "\n");
+  // out_stream << endl;
 }
 
 
 // function to calculate average price per category
-void pricePerCategory(vector<string> categories, vector<float> prices, ofstream& out_stream)
+// void pricePerCategory(vector<string> categories, vector<float> prices, ofstream& out_stream)
+void pricePerCategory(vector<string> categories, vector<float> prices, FILE *fptr)
 {
   // declare map to hold the average per category
   map<string, float> averages;
@@ -225,17 +254,25 @@ void pricePerCategory(vector<string> categories, vector<float> prices, ofstream&
   }
 
   // write the average for each category to the output file
-  out_stream << "\tCategory\t\tAverage\n";
+  // out_stream << "\tCategory\t\tAverage\n";
+  fprintf(fptr, "%s\t%s\n", "Category", "Average");
   for (itr = averages.begin(); itr != averages.end(); ++itr)
   {
-    out_stream << fixed << setprecision(2) << '\t' << itr->first << "\t\t" << itr->second << '\n';
+    // out_stream << fixed << setprecision(2) << '\t' << itr->first << "\t\t" << itr->second << '\n';
+    fprintf(fptr, "%s\t\t\t%4.2f\n", itr->first.c_str(), itr->second);
   }
-  out_stream << endl;
+
+  // print a new line
+    fprintf(fptr, "\n");
+
+  // out_stream << endl;
 }
 
 
 // function to count SKUs per year
-void skusPerYear(vector<int> years, vector<int> skus, ofstream& out_stream)
+void skusPerYear(vector<int> years, vector<int> skus, FILE *fptr)
+// void skusPerYear(vector<int> years, vector<int> skus, ofstream& out_stream)
+
 {
   // declare map to hold the years and skus
   map<int, vector<int> > skuMap;
@@ -264,22 +301,31 @@ void skusPerYear(vector<int> years, vector<int> skus, ofstream& out_stream)
   }
 
   // write the years, the skus, and the count for each year to the output file
-  out_stream << "\tYear\tSkus\n";
+  // out_stream << "\tYear\tSkus\n";
+  fprintf(fptr, "Year\tSkus\n");
   for (itr = skuMap.begin(); itr != skuMap.end(); ++itr)
   {
     int count = 0;
 
     // write each year to the output file
-    out_stream << '\t' << itr->first << '\t';
+    // out_stream << '\t' << itr->first << '\t';
+    fprintf(fptr, "%i\t", itr->first);
 
     // write the skus for each year to the output file
     for (int i = 0; i < itr->second.size(); i++)
     {
-      out_stream << setw(3) << setfill('0') << itr->second.at(i) << ' ';
+      // add padding zeros so skus are all 3 digits long
+      // out_stream << setw(3) << setfill('0') << itr->second.at(i) << ' ';
+      fprintf(fptr, "%03i ", itr->second.at(i));
       count++;
     }
+
     // write the count to the output file
-    out_stream << ' ' << "(Count: " << count << ")\n";
+    // out_stream << ' ' << "(Count: " << count << ")\n";
+    fprintf(fptr, "(Count: %i)", count);
+
+    // print a new line
+    fprintf(fptr, "\n");
   }
-  out_stream << endl;
+  // out_stream << endl;
 }
