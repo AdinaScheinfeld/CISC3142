83049f6c43860ccdde438d658789efca16fbac5c
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-03-17 12:32:02 -0400

    Move calculator creation to functions file

--- lab2_files/functions.h
+++ lab2_files/functions.h
@@ -1,7 +1,10 @@
 #ifndef functions_h
 #define functions_h
 
-// calculate function
+// calculate() function
 double calculate(Calculator myCalculator);
+
+// createCalculator() function
+Calculator createCalculator();
 
 #endif
--- lab2_files/main.cpp
+++ lab2_files/main.cpp
@@ -9,47 +9,8 @@ int main() {
     FILE * pFile;
     pFile = fopen("output.txt", "a");
 
-    // declare variable to hold the operator
-    Calculator::Operators o;
-
-    // get operator from user
-    char operatorInput;
-    printf("Please enter an operator "
-            "(use + for addition, - for subtraction, * for multiplication, / for division, "
-            "s for square root, or n for negation): ");
-    std::cin >> operatorInput;
-
-    // check to make sure that entered operator is valid
-    while(operatorInput != '+' && operatorInput != '-' && operatorInput != '*' && operatorInput != '/' 
-            && operatorInput != 's' && operatorInput != 'n') {
-        printf("Please enter a valid operator "
-                "(use + for addition, - for subtraction, * for multiplication, / for division, "
-                "s for square root, or n for negation): ");
-        std::cin >> operatorInput;
-    }
-
-    // assign the Calculator operation to the operation selected by the user
-    if(operatorInput == '+') {o = Calculator::Operators::a;}
-    else if(operatorInput == '-') {o = Calculator::Operators::s;}
-    else if(operatorInput == '*') {o = Calculator::Operators::m;}
-    else if(operatorInput == '/') {o = Calculator::Operators::d;}
-    else if(operatorInput == 's') {o = Calculator::Operators::r;}
-    else {o = Calculator::Operators::n;}
-
-    // get operands from user
-    double x = 0;
-    double y = 0;
-
-    printf("Please enter an operand: ");
-    std::cin >> x;
-
-    if(o != Calculator::Operators::s && o != Calculator::Operators::n) {
-        printf("Please enter another operand: ");
-        std::cin >> y;
-    }
-
-    // create instance of Calculator
-    Calculator myCalculator(o, x, y);
+    // create a Calculator using createCalculator() function
+    Calculator myCalculator = createCalculator();
 
     // pass Calculator to calculate()
     double result = calculate(myCalculator);
