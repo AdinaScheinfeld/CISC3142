0e649c693d4bc9436d4e07fb1cf045acd15d2adc
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-03-17 11:52:53 -0400

    Add main file

---
+++ lab2_files/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+#include "structures.h"
+#include "functions.h"
+
+int main() {
+
+    // declare variable to hold the operator
+    Calculator::Operators o;
+
+    // get operator from user
+    char operatorInput;
+    printf("Please enter an operator "
+            "(use + for addition, - for subtraction, * for multiplication, / for division, "
+            "s for square root, or n for negation): ");
+    std::cin >> operatorInput;
+
+    // check to make sure that entered operator is valid
+    while(operatorInput != '+' && operatorInput != '-' && operatorInput != '*' && operatorInput != '/' 
+            && operatorInput != 's' && operatorInput != 'n') {
+        printf("Please enter a valid operator "
+                "(use + for addition, - for subtraction, * for multiplication, / for division, "
+                "s for square root, or n for negation): ");
+        std::cin >> operatorInput;
+    }
+
+    // assign the Calculator operation to the operation selected by the user
+    if(operatorInput == '+') {o = Calculator::Operators::a;}
+    else if(operatorInput == '-') {o = Calculator::Operators::s;}
+    else if(operatorInput == '*') {o = Calculator::Operators::m;}
+    else if(operatorInput == '/') {o = Calculator::Operators::d;}
+    else if(operatorInput == 's') {o = Calculator::Operators::r;}
+    else {o = Calculator::Operators::n;}
+
+    // create instance of Calculator
+    Calculator myCalculator(o, 6.1, 2);
+
+    // pass Calculator to calculate()
+    double result = calculate(myCalculator);
+
+    // display result
+    printf("Your result is: %.2f\n.", result);
+
+    // indicate successful termination
+    return 0;
+}
