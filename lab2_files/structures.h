8657a7d70d21587a69ee6c3561103db033c93c14
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2022-03-17 11:50:11 -0400

    Add structures header file

---
+++ lab2_files/structures.h
@@ -0,0 +1,32 @@
+#ifndef structures_h
+#define structures_h
+
+#include <cmath>
+
+// Calculator data structure
+struct Calculator {
+
+    // enum of operators
+    // a: addition
+    // s: subtraction
+    // m: multiplication
+    // d: division
+    // r: square root
+    // n: negate
+    enum class Operators {a, s, m, d, r, n};
+
+    // Calculator member variables
+    Operators op;
+    double x;
+    double y;
+    double result;
+
+    // Calculator constructor
+    Calculator(Operators op0, double x0, double y0=0) {
+        op = op0;
+        x = x0;
+        y = y0;
+    }
+};
+
+#endif
